6a94180d872dcd4652f2c81a55ba8b98023b2a6e
kvlad <working03@rambler.ru>
2023-12-22 08:01:40 +0600

    add redirect btn

--- src/components/500/Page500.jsx
+++ src/components/500/Page500.jsx
@@ -1,12 +1,24 @@
 import "./Page500.scss";
 
 const Page500 = () => {
+
+    const handleLogin = () => {
+        window.location.href = 'https://vladnet.dev';
+    }
+
     return (
         <div className="st-page-not-found st-flex-center text-center">
-            <div className="">
+            <div className="container">
                 <h3>5<span>0</span>0</h3>
                 <h4>Website update is underway</h4>
                 <p>oops, you caught me updating my website.. Please try again later</p>
+                <div className="st-social-hero-info">
+                    <button
+                        className="st-btn st-style2 st-color1 st-size-large"
+                        onClick={handleLogin}
+                    >try again
+                    </button>
+                </div>
             </div>
         </div>
     )
